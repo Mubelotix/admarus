bcfa7a97c9a0d66c0d1fefd7a6523187011266f8
Mubelotix <mubelotix@gmail.com>
2023-07-05 23:06:18 +0200

    Refactor wordcount

--- daemon/src/result.rs
+++ daemon/src/result.rs
@@ -4,53 +4,86 @@ use crate::prelude::*;
 /// The sum of all fields is the total number of words.
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct WordCount {
-    #[serde(default)]
-    h1: usize,
-    #[serde(default)]
-    h2: usize,
-    #[serde(default)]
-    h3: usize,
-    #[serde(default)]
-    h4: usize,
-    #[serde(default)]
-    h5: usize,
-    #[serde(default)]
-    h6: usize,
-    /// Content with high importance
-    #[serde(default)]
-    strong: usize,
-    /// Content with some emphasis
-    #[serde(default)]
-    em: usize,
-    /// Normal text
-    #[serde(default)]
-    regular: usize,
-    /// Content with low importance
-    #[serde(default)]
-    small: usize,
-    /// No longer accurate or no longer relevant
-    #[serde(default)]
-    s: usize,
+    /// Counters for: h1, h2, h3, h4, h5, h6, strong, em, regular, small, s
+    data: [usize; 11]
 }
 
 impl WordCount {
-    #[allow(clippy::too_many_arguments)]
-    pub(crate) fn add(&mut self, h1: bool, h2: bool, h3: bool, h4: bool, h5: bool, h6: bool, strong: bool, em: bool, small: bool, s: bool) {
-        if h1 { self.h1 += 1; return }
-        if h2 { self.h2 += 1; return }
-        if h3 { self.h3 += 1; return }
-        if h4 { self.h4 += 1; return }
-        if h5 { self.h5 += 1; return }
-        if h6 { self.h6 += 1; return }
-        if strong { self.strong += 1; return }
-        if em { self.em += 1; return }
-        if small { self.small += 1; return }
-        if s { self.s += 1; return }
-        self.regular += 1;
+    pub const fn h1(&self) -> usize {
+        self.data[0]
+    }
+
+    pub const fn h2(&self) -> usize {
+        self.data[1]
+    }
+
+    pub const fn h3(&self) -> usize {
+        self.data[2]
+    }
+
+    pub const fn h4(&self) -> usize {
+        self.data[3]
+    }
+
+    pub const fn h5(&self) -> usize {
+        self.data[4]
+    }
+
+    pub const fn h6(&self) -> usize {
+        self.data[5]
+    }
+
+    pub const fn strong(&self) -> usize {
+        self.data[6]
+    }
+
+    pub const fn em(&self) -> usize {
+        self.data[7]
+    }
+
+    pub const fn regular(&self) -> usize {
+        self.data[8]
+    }
+
+    pub const fn small(&self) -> usize {
+        self.data[9]
+    }
+
+    pub const fn s(&self) -> usize {
+        self.data[10]
     }
 
     pub fn sum(&self) -> usize {
-        self.h1 + self.h2 + self.h3 + self.h4 + self.h5 + self.h6 + self.strong + self.em + self.regular + self.small + self.s
+        self.data.iter().sum()
+    }
+    
+    pub fn weighted_sum(&self) -> f64 {
+        self.h1() as f64 * 10.0
+            + self.h2() as f64 * 9.0
+            + self.h3() as f64 * 8.0
+            + self.h4() as f64 * 7.0
+            + self.h5() as f64 * 6.0
+            + self.h6() as f64 * 5.5
+            + self.strong() as f64 * 4.0
+            + self.em() as f64 * 1.1
+            + self.regular() as f64 * 1.0
+            + self.small() as f64 * 0.3
+            + self.s() as f64 * 0.1
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    pub(crate) fn add(&mut self, h1: bool, h2: bool, h3: bool, h4: bool, h5: bool, h6: bool, strong: bool, em: bool, small: bool, s: bool) {
+        if h1 { self.data[0] += 1; return }
+        if h2 { self.data[1] += 1; return }
+        if h3 { self.data[2] += 1; return }
+        if h4 { self.data[3] += 1; return }
+        if h5 { self.data[4] += 1; return }
+        if h6 { self.data[5] += 1; return }
+        if strong { self.data[6] += 1; return }
+        if em { self.data[7] += 1; return }
+        if small { self.data[9] += 1; return }
+        if s { self.data[10] += 1; return }
+        self.data[8] += 1;
     }
 }
 
