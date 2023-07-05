f6d143ed5177545380bb85c8ff352b9afdef826b
Mubelotix <mubelotix@gmail.com>
2023-07-05 13:30:32 +0200

    Add sorted_contains() on arrays

--- webui/src/util.rs
+++ webui/src/util.rs
@@ -72,3 +72,20 @@ impl<COMP: Component> HackTraitAnimation<COMP> for Scope<COMP> {
         })
     }
 }
+
+pub trait HackTraitSortedContains<T> {
+    /// Like `contains()` but optimized for sorted arrays.
+    fn sorted_contains(&self, item: T) -> bool;
+}
+
+impl<T: Ord> HackTraitSortedContains<T> for Vec<T> {
+    fn sorted_contains(&self, item: T) -> bool {
+        self.binary_search(&item).is_ok()
+    }
+}
+
+impl<T: Ord> HackTraitSortedContains<T> for [T] {
+    fn sorted_contains(&self, item: T) -> bool {
+        self.binary_search(&item).is_ok()
+    }
+}
