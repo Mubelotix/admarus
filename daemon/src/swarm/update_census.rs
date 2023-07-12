ae06ff20df0f40b55811fe6fbfb32f02f1126a1c
Mubelotix <mubelotix@gmail.com>
2023-07-12 22:19:13 +0200

    Fix too many folders for census

--- daemon/src/swarm/update_census.rs
+++ daemon/src/swarm/update_census.rs
@@ -23,15 +23,20 @@ pub async fn update_census_task<const N: usize>(node: NodeController, index: Doc
         external_addrs.extend(new_addrs);
         
         if external_addrs.is_empty() {
-            sleep(Duration::from_secs(30*60)).await;
             warn!("Failed to advertise ourselves to census due to lack of known external addresses");
+            sleep(Duration::from_secs(30*60)).await;
             continue;
         }
 
+        let mut folders = index.folders().await.into_iter().map(|(cid, count)| (cid, count as u64)).collect::<Vec<_>>();
+        if folders.len() > 500 {
+            folders.sort_by(|(_, a), (_, b)| b.cmp(a));
+            folders.truncate(500);
+        }
         let record = Record {
             peer_id: keypair.public().to_peer_id().to_string(),
             addrs: external_addrs.clone(),
-            folders: index.folders().await.into_iter().map(|(cid, count)| (cid, count as u64)).collect(),
+            folders,
         };
 
         match submit_census_record(&config.census_rpc, record, keypair.clone()).await {
