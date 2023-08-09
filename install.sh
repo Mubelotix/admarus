89bb7525f52e0b47c35cff8cd205acd5e4731878
Mubelotix <mubelotix@gmail.com>
2023-08-10 01:28:27 +0200

    Fix for mac

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+green='\033[0;32m'
+red='\033[1;31m'
+normal='\033[0m'
+
 # This script uses the Rustup script as a library to detect the architecture of the system
 source_rustup_functions() {
     echo "[1/3] Downloading library"
     rust_script=$(curl --proto '=https' --tlsv1.2 -sS https://sh.rustup.rs)
     if [ $? -ne 0 ]; then
-        echo "Error: Failed to download library"
+        echo "${red}Error: Failed to download library${normal}"
         return 1
     fi
 
     last_line=$(echo "$rust_script" | tail -n 1)
     if [ "$last_line" != 'main "$@" || exit 1' ]; then
-        echo "Error: An update to the Rustup script has broken this script. Please open an issue at https://github.com/Mubelotix/admarus/issues"
+        echo "${red}Error: An update to the Rustup script has broken this script.${normal} Please open an issue at https://github.com/Mubelotix/admarus/issues"
         return 1
     fi
 
@@ -22,7 +26,6 @@ source_rustup_functions() {
 }
 
 source_rustup_functions
-set -e
 
 get_architecture
 arch="$RETVAL"
@@ -33,11 +36,14 @@ download_url="https://github.com/mubelotix/admarus/releases/download/$version/ad
 
 echo "[2/3] Downloading admarusd $version"
 curl --fail --location --progress-bar "$download_url" -o "/tmp/$filename"
+case $? in
+    0)  ;;
+    22) echo "${red}No available binary for your system.${normal} Please build from source: https://github.com/Mubelotix/admarus/wiki/building-from-source"; exit 1 ;;
+    *)  echo "${red}Error: Failed to download admarusd binary${normal}"; exit 1 ;;
+esac
 
 echo "[3/3] Installing admarusd at /user/local/bin/admarusd"
 sudo mv "/tmp/$filename" "/usr/local/bin/admarusd"
 chmod +x "/usr/local/bin/admarusd"
 
-green='\033[0;32m'
-normal='\033[0m'
 echo "${green}Admarus $version has been installed successfully!${normal}"
